45ce5303afa015eb21b1078d7a856b86ace9bbbe
Allen <allenliu@outlook.com>
2026-09-08 09:22:09 -0500

    First Commit.

---
+++ CalculateSomethingApp/AnswerView.swift
@@ -0,0 +1,23 @@
+//
+//  AnswerView.swift
+//  CalculateSomethingApp
+//
+//  Created by ALLEN LIU on 9/7/26.
+//
+
+import SwiftUI
+
+struct AnswerView: View {
+    
+    @State var answer: Double
+    
+    var body: some View {
+        
+        
+        
+    }
+}
+
+#Preview {
+    
+}
--- CalculateSomethingApp/ContentView.swift
+++ CalculateSomethingApp/ContentView.swift
@@ -9,16 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var weight = ""
-    @State var DWeight = 0.0
-    @State var Fn = 0.0
+    @State var Side1 = ""
+    @State var Side2 = ""
+    @State var DSide1 = 0.0
+    @State var DSide2 = 0.0
+    @State var answer = 0.0
     @State var showAlert = false
-    func FindFn(){
-        if let DWeight = Double(weight){
-            Fn = 9.8 * DWeight
+//    @State var SquareNAdd = 0.0
+    
+    func FindTriangle(){
+        if let side1 = Double(Side1){
+            DSide1 = side1
         } else {
             showAlert.toggle()
         }
+        if let side2 = Double(Side2){
+            DSide2 = side2
+        } else {
+            showAlert.toggle()
+        }
+        
+        answer = sqrt(pow(DSide1, 2) + pow(DSide2, 2))
     }
     
     
@@ -28,11 +39,28 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            ZStack(alignment: .top){
+                Text("Pythagorean Theorem")
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .font(.largeTitle)
+                    .bold()
+                    .shadow(color: .yellow , radius: 12)
+                    .background(
+                        RoundedRectangle(cornerRadius: 20)
+                        .frame(width:300, height: 80)
+                        .foregroundStyle(.green))
+                
+                
+            }
             
-            ZStack{
-            //    RoundedRectangle(cornerRadius: 25)
-             //       .frame(width: 150, height: 50)
-                TextField("Enter weight in pounds", text:$weight)
+            Spacer()
+            
+            VStack{
+                
+            }
+            
+            TextField("Side 1 ", text:$Side1)
                     .textFieldStyle(.plain)
                     .padding()
                     .background(Color(.systemBackground))
@@ -43,13 +71,39 @@ struct ContentView: View {
                         )
                     )
 
+            TextField("Side 2 ", text:$Side2)
+                .textFieldStyle(.plain)
+                .padding()
+                .background(Color(.systemBackground))
+                .clipShape(
+                    RoundedRectangle(
+                        cornerSize: CGSize(width: 20, height: 20),
+                        style: .continuous
+                    )
+                )
+            
+            
+            NavigationLink{
+                AnswerView(answer:answer)
+            } label: {
+                ZStack{
+                    Text("Review Length of 3rd side.")
+                        .font(.default)
+                        .textFieldStyle(.plain)
+                        .padding()
+                        .multilineTextAlignment(.center)
+                        .background(
+                            RoundedRectangle(cornerRadius:15)
+                                .frame(width: 250, height: 50)
+                                .foregroundStyle(.green)
+                        )
+                        
+                }
                 
             }
             
-            
-            
-            
-            
+            Spacer()
+            Spacer()
             
             
         }
@@ -59,7 +113,8 @@ struct ContentView: View {
         .background(.purple)
         .alert("Put the numbers in not the letters ", isPresented: $showAlert, actions:{
             Button("I understand I have failed"){
-                weight = ""
+                Side1 = ""
+                Side2 = ""
             }
         })
     }
